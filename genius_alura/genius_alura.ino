834b39dd648fe0736a247bfae8926d1c80e5275a
Andre Chaves <andrechavesg@gmail.com>
2018-04-29 20:55:40 -0300

    capitulo7

--- genius_alura/genius_alura.ino
+++ genius_alura/genius_alura.ino
@@ -1,20 +1,71 @@
+#define LED_VERDE 2
+#define LED_AMARELO 3
 #define LED_VERMELHO 4
+#define LED_AZUL 5
+
+#define BOTAO_VERDE 8
+#define BOTAO_AMARELO 9
 #define BOTAO_VERMELHO 10
+#define BOTAO_AZUL 11
 
-void setup() {
-  Serial.begin(9600);
-  pinMide(LED_VERMELHO,OUTPUT);
+#define UM_SEGUNDO 1000
+#define MEIO_SEGUNDO 500
 
-  pinMode(BOTAO_VERMELHO,INPUT_PULLUP);
+#define TAMANHO_SEQUENCIA 4
+#define INDEFINIDO -1
+int sequenciaLuzes[TAMANHO_SEQUENCIA];
 
-  digitalWrite(LED_VERMELHO,HIGH);
-  delay(1000);
-  digitalWrite(LED_VERMELHO,LOW);
+
+void setup(){
+    Serial.begin(9600);
+    iniciaPortas();
+    iniciaJogo();
 }
 
-void loop() {
-  int estadoBotao = digitalRead(BOTAO_VERMELHO);
-  Seriail.println(estadoBotao);
-  delay(500);
+void iniciaJogo(){
+  sequenciaLuzes[0] = LED_AZUL;
+  sequenciaLuzes[1] = LED_VERDE;
+  sequenciaLuzes[2] = LED_VERMELHO;
+  sequenciaLuzes[3] = LED_AMARELO;
 }
 
+void iniciaPortas(){
+    pinMode(LED_VERDE, OUTPUT);
+    pinMode(LED_AMARELO, OUTPUT);
+    pinMode(LED_VERMELHO, OUTPUT);
+    pinMode(LED_AZUL, OUTPUT);
+
+    pinMode(BOTAO_VERDE, INPUT_PULLUP);
+    pinMode(BOTAO_AMARELO, INPUT_PULLUP);
+    pinMode(BOTAO_VERMELHO, INPUT_PULLUP);
+    pinMode(BOTAO_AZUL, INPUT_PULLUP);
+}
+
+
+void loop(){
+    Serial.println(checaRespostaJogador());
+}
+
+int checaRespostaJogador() {
+  if(digitalRead(BOTAO_VERDE) == LOW) {
+     return piscaLed(LED_VERDE);  
+  }
+  if(digitalRead(BOTAO_AMARELO) == LOW) {
+     return piscaLed(LED_AMARELO);  
+  }
+  if(digitalRead(BOTAO_VERMELHO) == LOW) {
+     return piscaLed(LED_VERMELHO);  
+  }
+  if(digitalRead(BOTAO_AZUL) == LOW) {
+     return piscaLed(LED_AZUL);  
+  }
+  return INDEFINIDO;
+}
+
+int piscaLed(int portaLed){
+  digitalWrite(portaLed,HIGH);
+  delay(UM_SEGUNDO); 
+  digitalWrite(portaLed,LOW);
+  delay(MEIO_SEGUNDO);
+  return portaLed;
+}

57f2f1206ae388bbe73ce02a466b2a873c54dace
Andre Chaves <andrechavesg@gmail.com>
2018-04-29 19:24:06 -0300

    capitulo4

--- genius_alura/genius_alura.ino
+++ genius_alura/genius_alura.ino
@@ -1,18 +1,40 @@
-//int ledVermelho = 2;
-//declarando a constante
+#define LED_VERDE 2
+#define LED_AMARELO 3
+#define LED_VERMELHO 4
+#define LED_AZUL 5
 
-#define LED_VERMELHO 2
 #define UM_SEGUNDO 1000
 #define MEIO_SEGUNDO 500
 
+#define TAMANHO_SEQUENCIA 4
+int sequenciaLuzes[TAMANHO_SEQUENCIA];
+
+
 void setup(){
     Serial.begin(9600);
-    pinMode(LED_VERMELHO, OUTPUT);
+    iniciaPortas();
+    iniciaJogo();
 }
 
+void iniciaJogo(){
+  sequenciaLuzes[0] = LED_AZUL;
+  sequenciaLuzes[1] = LED_VERDE;
+  sequenciaLuzes[2] = LED_VERMELHO;
+  sequenciaLuzes[3] = LED_AMARELO;
+}
+
+void iniciaPortas(){
+    pinMode(LED_VERDE, OUTPUT);
+    pinMode(LED_AMARELO, OUTPUT);
+    pinMode(LED_VERMELHO, OUTPUT);
+    pinMode(LED_AZUL, OUTPUT);
+}
+
+
 void loop(){
-    Serial.println(LED_VERMELHO);
-    piscaLed(LE D_VERMELHO); //estamos chamando a função
+    for(int indice = 0; indice < TAMANHO_SEQUENCIA; indice++){
+        piscaLed(sequenciaLuzes[indice]);
+    }
 }
 
 void piscaLed(int portaLed){

e9f72bca5a7a2f3d8d0e8aeedc8c3b8bb5dbbdb0
Rodrigo da Silva Ferreira Caneppele <r.caneppele@gmail.com>
2024-04-29 17:10:28 -0300

    Ajuste no código genius_alura.ino

--- genius_alura/genius_alura.ino
+++ genius_alura/genius_alura.ino
@@ -3,6 +3,8 @@
 #define LED_VERMELHO 4
 #define LED_AZUL 5
 
+#define INDEFINIDO -1
+
 #define BOTAO_VERDE 8
 #define BOTAO_AMARELO 9
 #define BOTAO_VERMELHO 10
@@ -12,65 +14,134 @@
 #define MEIO_SEGUNDO 500
 
 #define TAMANHO_SEQUENCIA 4
-#define INDEFINIDO -1
+
+enum Estados {
+  PRONTO_PARA_PROXIMA_RODADA,
+  USUARIO_RESPONDENDO,
+  JOGO_FINALIZADO_SUCESSO,
+  JOGO_FINALIZADO_FALHA
+};
+
 int sequenciaLuzes[TAMANHO_SEQUENCIA];
 
+int rodada = 0;
 
-void setup(){
-    Serial.begin(9600);
-    iniciaPortas();
-    iniciaJogo();
+void setup() {
+  Serial.begin(9600);
+
+  iniciaPortas();
+  iniciaJogo();
+}
+
+void iniciaPortas() {
+  pinMode(LED_VERDE, OUTPUT);
+  pinMode(LED_AMARELO, OUTPUT);
+  pinMode(LED_VERMELHO, OUTPUT);
+  pinMode(LED_AZUL, OUTPUT);
+
+  pinMode(BOTAO_VERDE, INPUT_PULLUP);
+  pinMode(BOTAO_AMARELO, INPUT_PULLUP);
+  pinMode(BOTAO_VERMELHO, INPUT_PULLUP);
+  pinMode(BOTAO_AZUL, INPUT_PULLUP);
 }
 
 void iniciaJogo() {
-  int potenciometro = analogRead(0);
-  Serial.println(potenciometro);
-  for(int indice = 0; indice < TAMANHO_SEQUENCIA; indice ++){
-    sequenciaLuzes[indice] = sorteiaCor();
+  int jogo = analogRead(0);
+  randomSeed(jogo);
+
+  for (int i = 0; i < TAMANHO_SEQUENCIA; i++) {
+    sequenciaLuzes[i] = sorteiaCor();
   }
 }
 
 int sorteiaCor() {
-    return random(LED_VERDE, LED_AZUL + 1); // 2-5 (6)
+  return random(LED_VERDE, LED_AZUL + 1);
 }
 
-void iniciaPortas(){
-    pinMode(LED_VERDE, OUTPUT);
-    pinMode(LED_AMARELO, OUTPUT);
-    pinMode(LED_VERMELHO, OUTPUT);
-    pinMode(LED_AZUL, OUTPUT);
+void loop() {
+  switch (estadoAtual()) {
+    case PRONTO_PARA_PROXIMA_RODADA:
+      Serial.println("Pronto para a proxima rodada");
+      preparaNovaRodada();
+      break;
+    case USUARIO_RESPONDENDO:
+      Serial.println("Usuario respondendo");
+      break;
+    case JOGO_FINALIZADO_SUCESSO:
+      Serial.println("Jogo finalizado com sucesso");
+      break;
+    case JOGO_FINALIZADO_FALHA:
+      Serial.println("Jogo finalizado com falha");
+      break;
+  }
+  delay(MEIO_SEGUNDO);
 
-    pinMode(BOTAO_VERDE, INPUT_PULLUP);
-    pinMode(BOTAO_AMARELO, INPUT_PULLUP);
-    pinMode(BOTAO_VERMELHO, INPUT_PULLUP);
-    pinMode(BOTAO_AZUL, INPUT_PULLUP);
 }
 
+void preparaNovaRodada() {
+  rodada++;
+  tocaLedsRodada();
+}
 
-void loop(){
-    Serial.println(checaRespostaJogador());
+int estadoAtual() {
+  if (rodada < TAMANHO_SEQUENCIA) {
+      return PRONTO_PARA_PROXIMA_RODADA; 
+  } else {
+    return JOGO_FINALIZADO_SUCESSO;
+  } 
+}
+
+void tocaLedsRodada() {
+  for (int i = 0; i < rodada; i++) {
+    piscaLed(sequenciaLuzes[i]);
+  }
 }
 
 int checaRespostaJogador() {
-  if(digitalRead(BOTAO_VERDE) == LOW) {
-     return piscaLed(LED_VERDE);  
+  if (digitalRead(BOTAO_VERDE) == LOW) {
+    return piscaLed(LED_VERDE);
   }
-  if(digitalRead(BOTAO_AMARELO) == LOW) {
-     return piscaLed(LED_AMARELO);  
+  if (digitalRead(BOTAO_AMARELO) == LOW) {
+    return piscaLed(LED_AMARELO);
   }
-  if(digitalRead(BOTAO_VERMELHO) == LOW) {
-     return piscaLed(LED_VERMELHO);  
+  if (digitalRead(BOTAO_VERMELHO) == LOW) {
+    return piscaLed(LED_VERMELHO);
   }
-  if(digitalRead(BOTAO_AZUL) == LOW) {
-     return piscaLed(LED_AZUL);  
+  if (digitalRead(BOTAO_AZUL) == LOW) {
+    return piscaLed(LED_AZUL);
   }
+
   return INDEFINIDO;
 }
 
-int piscaLed(int portaLed){
+
+void piscaSequencia1() {
+  piscaLed(LED_VERDE);
+  piscaLed(LED_AMARELO);
+  piscaLed(LED_VERMELHO);
+  piscaLed(LED_AZUL);
+  delay(MEIO_SEGUNDO);
+}
+
+void piscaSequencia2() {
+  digitalWrite(LED_VERDE,HIGH);
+  digitalWrite(LED_AMARELO,HIGH);
+  digitalWrite(LED_VERMELHO,HIGH);
+  digitalWrite(LED_AZUL,HIGH);
+  delay(UM_SEGUNDO);
+  digitalWrite(LED_VERDE,LOW);
+  digitalWrite(LED_AMARELO,LOW);
+  digitalWrite(LED_VERMELHO,LOW);
+  digitalWrite(LED_AZUL,LOW);
+  delay(MEIO_SEGUNDO);
+}
+
+int piscaLed(int portaLed) {
+
   digitalWrite(portaLed,HIGH);
-  delay(UM_SEGUNDO); 
+  delay(UM_SEGUNDO);
   digitalWrite(portaLed,LOW);
   delay(MEIO_SEGUNDO);
+
   return portaLed;
 }
